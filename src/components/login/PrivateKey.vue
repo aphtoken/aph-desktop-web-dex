3782ab7b577f64b8c55a6dc480ae2c8f25087456
Eddie Harrison <eddie@eddieharrison.co.uk>
2019-03-06 12:26:53 +0000

    Amend double login back to on-submit rather than buttton@click to enable pressing enter to login.

--- src/components/login/PrivateKey.vue
+++ src/components/login/PrivateKey.vue
@@ -1,8 +1,8 @@
 <template>
   <section id="login--saved-wallet">
-    <login-form-wrapper identifier="openPrivateKey">
+    <login-form-wrapper identifier="openPrivateKey" :on-submit="login">
       <aph-input :hasError="$isFailed('openPrivateKey')" v-model="wif" :placeholder="$t('enterYourPrivateKeyWIF')" type="password"></aph-input>
-      <button class="login" @click="login" :disabled="shouldDisableLoginButton">{{$t('login')}}</button>
+      <button class="login" :disabled="shouldDisableLoginButton">{{$t('login')}}</button>
     </login-form-wrapper>
   </section>
 </template>
